710c8a12492992f3ef3c7fcd3199cf1f5a0c2971
Greg Stead <gregstead@gmail.com>
2021-05-16 15:05:57 -0500

    adds express server

---
+++ server/server.js
@@ -0,0 +1,31 @@
+// Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+//Express app
+const app = express();
+
+//Express configurations
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Allow express app to serve static content from "client"
+app.use(express.static("client"));
+
+// Connect to MongoDB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/ticketopen_db",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
+
+// Listen
+app.listen(PORT, () => {
+  console.log(`App listening on port: ${PORT}`);
+});
