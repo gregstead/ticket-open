800ab96275adb57ffcc2c5e2bbb94492ef2887f7
Greg Stead <gregstead@gmail.com>
2021-05-16 13:46:07 -0500

    removes react files

--- server/models/User.js
+++
@@ -1,9 +0,0 @@
-const { model, Schema } = require('mongoose');
-// https://www.npmjs.com/package/passport-local-mongoose
-const passportLocalMongoose = require('passport-local-mongoose');
-
-const User = new Schema({});
-
-User.plugin(passportLocalMongoose);
-
-module.exports = model('User', User);
