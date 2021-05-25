04c6bbe843d96389fff066149961ac4f5bc4ccad
Greg Stead <gregstead@gmail.com>
2021-05-25 07:33:33 -0500

    adds methods to front end api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,14 +2,24 @@ import axios from "axios";
 
 const API = {
   // Patron methods
-  // Get patron
-  getAllPatrons: function (id) {
+  // Add one patron
+  addPatron: function (patron) {
+    return axios.post("/patron", patron);
+  },
+  // Get patrons
+  getAllPatrons: function () {
     let url = "/patron/";
     return axios.get(url);
   },
-  // Add one patron
-  addOnePatron: function (patron) {
-    return axios.post("/patron", patron);
+  // Get patron by id
+  getPatron: function (id) {
+    let url = "/patron/" + id;
+    return axios.get(url);
+  },
+  // Update one patron
+  updateOnePatron: function (id, patron) {
+    let url = "/patron/" + id;
+    return axios.put(url, patron);
   },
 };
 
