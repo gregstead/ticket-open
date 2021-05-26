6fba0a82260365d555d2d1c7b231e13c045d484a
Greg Stead <gregstead@gmail.com>
2021-05-26 18:56:15 -0500

    refactors database

--- server/models/user.js
+++ server/models/user.js
@@ -2,9 +2,42 @@ const { model, Schema } = require("mongoose");
 const validator = require("validator");
 
 const userSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "Person",
+  // User Name
+  title: String,
+  firstName: String,
+  middleName: String,
+  lastName: String,
+  salutation: String,
+  // User Address
+  streetNumber: String,
+  buildingName: String,
+  streetNumberSuffix: String,
+  streetName: String,
+  streetType: String,
+  streetDirection: String,
+  addressType: String,
+  addressTypeIdentifier: String,
+  localMunicipality: String,
+  city: String,
+  governingDistrict: String,
+  postalArea: String,
+  country: String,
+  // Patron Contact info
+  phone: String,
+  mobile: {
+    type: String,
+    validate: validator.isMobilePhone,
+    message: "That is not a valid mobile phone number",
+    isAsync: false,
+    required: false,
+  },
+  email: {
+    type: String,
+    unique: true,
+    validate: validator.isEmail,
+    message: "That is not a valid email",
+    isAsync: false,
+    required: true,
   },
   department: String,
   password: String,
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const addressRoutes = require("./address");
 const businessRoutes = require("./business");
 const noteRoutes = require("./note");
 const patronRoutes = require("./patron");
-const personRoutes = require("./person");
 const userRoutes = require("./user");
 
 // Patron routes
-router.use("/address", addressRoutes);
 router.use("/business", businessRoutes);
 router.use("/note", noteRoutes);
 router.use("/patron", patronRoutes);
-router.use("/person", personRoutes);
 router.use("/user", userRoutes);
 
 module.exports = router;
