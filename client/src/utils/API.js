81e7405d6b273f0df845a7ca6391f16507f2033d
Greg Stead <gregstead@gmail.com>
2021-05-30 14:53:04 -0500

    Revert "adds methods to front end api"
    This reverts commit 04c6bbe843d96389fff066149961ac4f5bc4ccad.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,24 +2,14 @@ import axios from "axios";
 
 const API = {
   // Patron methods
-  // Add one patron
-  addPatron: function (patron) {
-    return axios.post("/patron", patron);
-  },
-  // Get patrons
-  getAllPatrons: function () {
+  // Get patron
+  getAllPatrons: function (id) {
     let url = "/patron/";
     return axios.get(url);
   },
-  // Get patron by id
-  getPatron: function (id) {
-    let url = "/patron/" + id;
-    return axios.get(url);
-  },
-  // Update one patron
-  updateOnePatron: function (id, patron) {
-    let url = "/patron/" + id;
-    return axios.put(url, patron);
+  // Add one patron
+  addOnePatron: function (patron) {
+    return axios.post("/patron", patron);
   },
 };
 
