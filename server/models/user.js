c1ec226ac61ecdaa52a4eb9cd3a5687bb783159d
Greg Stead <gregstead@gmail.com>
2021-05-30 14:16:54 -0500

    Revert "refactors database"
    This reverts commit 6fba0a82260365d555d2d1c7b231e13c045d484a.

--- server/models/user.js
+++ server/models/user.js
@@ -2,42 +2,9 @@ const { model, Schema } = require("mongoose");
 const validator = require("validator");
 
 const userSchema = new Schema({
-  // User Name
-  title: String,
-  firstName: String,
-  middleName: String,
-  lastName: String,
-  salutation: String,
-  // User Address
-  streetNumber: String,
-  buildingName: String,
-  streetNumberSuffix: String,
-  streetName: String,
-  streetType: String,
-  streetDirection: String,
-  addressType: String,
-  addressTypeIdentifier: String,
-  localMunicipality: String,
-  city: String,
-  governingDistrict: String,
-  postalArea: String,
-  country: String,
-  // Patron Contact info
-  phone: String,
-  mobile: {
-    type: String,
-    validate: validator.isMobilePhone,
-    message: "That is not a valid mobile phone number",
-    isAsync: false,
-    required: false,
-  },
-  email: {
-    type: String,
-    unique: true,
-    validate: validator.isEmail,
-    message: "That is not a valid email",
-    isAsync: false,
-    required: true,
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "Person",
   },
   department: String,
   password: String,
