ce27af57f04f9587d39eb1e6edad00d0d6aa0259
Greg Stead <gregstead@gmail.com>
2021-05-31 04:22:54 -0500

    adds addUser form

---
+++ client/src/pages/AddNewUser/index.jsx
@@ -0,0 +1,257 @@
+import React, { useState } from "react";
+import API from "../../utils/API";
+
+function UserForm() {
+  const [userState, setUserState] = useState({
+    title: "",
+    firstName: "",
+    middleName: "",
+    lastName: "",
+    salutation: "",
+    address: {
+      streetNumber: "",
+      buildingName: "",
+      streetNumberSuffix: "",
+      streetName: "",
+      streetType: "",
+      streetDirection: "",
+      addressType: "",
+      addressTypeIdentifier: "",
+      localMunicipality: "",
+      city: "",
+      governingDistrict: "",
+      postalArea: "",
+      country: "",
+    },
+    phone: "",
+    mobile: "",
+    email: "",
+    role: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setUserState({ ...userState, [name]: value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    API.createNewUser(userState);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Title:
+        <select name="title" value={userState.title} onChange={handleChange}>
+          <option selected value="none"></option>
+          <option value="mr">Mr.</option>
+          <option value="ms">Ms.</option>
+          <option value="mrs">Mrs.</option>
+          <option value="dr">Dr.</option>
+        </select>
+      </label>
+      <label>
+        First name:
+        <input
+          type="text"
+          name="firstName"
+          value={userState.firstName}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        middle name:
+        <input
+          type="text"
+          name="middleName"
+          value={userState.middleName}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        last name:
+        <input
+          type="text"
+          name="lastName"
+          value={userState.lastName}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Salutation:
+        <input
+          type="text"
+          name="salutation"
+          value={userState.salutation}
+          onChange={handleChange}
+        />
+      </label>
+      {/*// User Address*/}
+      <label>
+        Street number:
+        <input
+          type="text"
+          name="streetNumber"
+          value={userState.streetNumber}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Building Name:
+        <input
+          type="text"
+          name="buildingName"
+          value={userState.buildingName}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Street number suffix:
+        <input
+          type="text"
+          name="streetNumberSuffix"
+          value={userState.streetNumberSuffix}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Street name:
+        <input
+          type="text"
+          name="streetName"
+          value={userState.streetName}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Street type:
+        <input
+          type="text"
+          name="streetType"
+          value={userState.streetType}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Street direction:
+        <select
+          name="streetDirection"
+          value={userState.streetDirection}
+          onChange={handleChange}
+        >
+          <option selected value="none"></option>
+          <option value="n">N</option>
+          <option value="ne">NE</option>
+          <option value="e">E</option>
+          <option value="se">SE</option>
+          <option value="s">S</option>
+          <option value="sw">SW</option>
+          <option value="W">W</option>
+          <option value="nw">NW</option>
+        </select>
+      </label>
+      <label>
+        Address type:
+        <select
+          name="addressType"
+          value={userState.addressType}
+          onChange={handleChange}
+        >
+          <option selected value="none"></option>
+          <option value="home">Home</option>
+          <option value="apt">Apartment</option>
+          <option value="fl">Floor</option>
+          <option value="ste">Suite</option>
+          <option value="pobox">PO Box</option>
+          <option value="office">Office</option>
+          <option value="unit">Unit</option>
+        </select>
+      </label>
+      <label>
+        Municipality:
+        <input
+          type="text"
+          name="localMunicipality"
+          value={userState.localMunicipality}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        City:
+        <input
+          type="text"
+          name="city"
+          value={userState.city}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        State:
+        <input
+          type="text"
+          name="giverningDistrict"
+          value={userState.giverningDistrict}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Zip/Postal code:
+        <input
+          type="text"
+          name="postalArea"
+          value={userState.postalArea}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Country:
+        <input
+          type="text"
+          name="country"
+          value={userState.country}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Home phone :
+        <input
+          type="text"
+          name="phone"
+          value={userState.phone}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Mobile phone:
+        <input
+          type="text"
+          name="mobile"
+          value={userState.mobile}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        email:
+        <input
+          type="text"
+          name="email"
+          value={userState.email}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Role:
+        <input
+          type="text"
+          name="role"
+          value={userState.role}
+          onChange={handleChange}
+        />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
+
+export default UserForm;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,22 +7,37 @@ const API = {
   },
   // POST	'/patron'	--	create a new patron
   createNewPatron(patron) {
-    axios
-      .post("/patron/", patron)
-      .then((data) => {
-        return data;
-      })
-      .catch((err) => console.error(err));
+    return axios.post("/patron/", patron).catch((err) => console.error(err));
   },
-  // Create a new patron
-
   // GET	'/patron/:id'	/patron/show	display a specific patron
-
+  getPatron(id) {
+    const url = "/patron/" + id;
+    return axios.get(url);
+  },
   // GET	'/patron/:id/edit'	/patron/edit	return an HTML form for editing a patron
-
+  updatePatron(id, patron) {
+    const url = "/patron/" + id + "/edit";
+    return axios.put(url, patron);
+  },
   // PUT	'/patron/:id'	--	update a specific patron
 
   // DELETE	'/patron/:id'	--	delete a specific patron
+
+  // Method url-path view-file description
+  // GET	'/user'	/user/index	display a list of all users
+  // GET	'/user/new'	/user/new	return an HTML form for creating a new user
+  addUser(user) {
+    return axios.post("/user", user);
+  },
+  // POST	'/user'	--	create a new user
+
+  // GET	'/user/:id'	/user/show	display a specific user
+
+  // GET	'/user/:id/edit'	/user/edit	return an HTML form for editing a user
+
+  // PUT	'/user/:id'	--	update a specific user
+
+  // DELETE	'/user/:id'	--	delete a specific user
 };
 
 export default API;
