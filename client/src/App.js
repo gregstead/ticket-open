5ada95b68a93ca9257646147e9e56206aaea3d53
Greg Stead <gregstead@gmail.com>
2021-06-01 21:20:04 -0500

    adds router

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,26 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Switch } from "react-router-dom";
-import PDrawer from "./components/PDrawer";
-import AddNewPatron from "./pages/AddNewPatron";
-import Patrons from "./pages/Patrons";
-import Login from "./pages/Login";
-import { userContext } from "./userContext";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LeftDrawer from "./components/LeftDrawer";
+import routes from "./routes";
 
 function App() {
   return (
     <Router>
-      <div>
-        <PDrawer
-          children={
-            <Switch>
-              <Route exact path="/" component={Login} />
-              <Route exact path="/patron/new" component={AddNewPatron} />
-              <Route exact path="/patron" component={Patrons} />
-            </Switch>
-          }
-        />
-      </div>
+      <LeftDrawer>
+        <Switch>
+          {routes.map((route, key) => {
+            return (
+              <Route
+                exact
+                path={route.path}
+                component={route.component}
+                key={key}
+              />
+            );
+          })}
+        </Switch>
+      </LeftDrawer>
     </Router>
   );
 }
