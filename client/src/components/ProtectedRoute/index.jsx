bd652a1c538363f6f9d870a7ad5bf7918f6dee00
Greg Stead <gregstead@gmail.com>
2021-06-02 00:22:19 -0500

    Adds protected routes

--- client/src/components/ProtectedRoute/index.jsx
+++ client/src/components/ProtectedRoute/index.jsx
@@ -1,22 +1,21 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-// import { useAuth } from "../../userContext";
+import { useAuth } from "../../userContext";
 
 function ProtectedRoute({ component: Component, ...rest }) {
   // usercontext at the global level -- inject usercontext and see if they're signed in
   // determine how they will be authenticated
-  //   const isAuthenticated = useAuth();
+  const isAuthenticated = useAuth();
 
   return (
     <Route
       {...rest}
       render={(props) => {
-        return <Component {...props} />;
-        // return isAuthenticated.authTokens.id ? (
-        //   <Component {...props} />
-        // ) : (
-        //   <Redirect to="/login" />
-        // );
+        return isAuthenticated.authTokens.id ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        );
       }}
     />
   );
