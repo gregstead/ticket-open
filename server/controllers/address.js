75001d1256cfa3b1d438f6bb7eb5377e85e9167b
Greg Stead <gregstead@gmail.com>
2021-06-06 10:49:51 -0500

    ads methods to address

--- server/controllers/address.js
+++ server/controllers/address.js
@@ -6,6 +6,27 @@ const address = {
       .then((result) => res.json(result))
       .catch((err) => res.status(500).json(err));
   },
+  findAddressById: function(req, res) {
+    db.Address.findById(req.params.id)
+      .then((result) => res.json(result))
+      .catch((err) => res.status(500).json(err));
+  },
+  createNewAddress: function(req, res) {
+    db.Address.create(req.body)
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+  updateUser: async function(req, res) {
+    db.User.update(req.body, {
+      where: {
+        id: id,
+      },
+    })
+      .then((result) => res.json(result))
+      .catch((err) => res.status(500).json(err));
+  },
 };
 
 module.exports = address;
