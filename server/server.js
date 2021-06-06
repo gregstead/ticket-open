596511ffcab7c1ad63f09589484f877b9bbdc639
Greg Stead <gregstead@gmail.com>
2021-06-05 20:29:57 -0500

    adds helmet, session, cors

--- server/server.js
+++ server/server.js
@@ -1,34 +1,58 @@
-// Dependencies
-require("dotenv").config();
+const path = require("path");
 const express = require("express");
-
-const routes = require("./routes");
-
-const PORT = process.env.PORT || 3001;
-const SYNC_OPTIONS = {
-  force: process.env.NODE_ENV === "test",
-};
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session");
 
 const db = require("./models");
+const routes = require("./routes");
+const passport = require("./config/passport");
+const corsOptions = require("./config/cors.js");
 
-//Express app
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-//Express configurations
+// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(helmet());
+app.use(session({ secret: "TBD", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cors(corsOptions));
 
-// Allow express app to serve static content from "client"
-app.use(express.static("client"));
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../client/build"));
+}
 
-// add routes, API
+// Add routes, API
 app.use(routes);
 
-// Connect to mysql db
-
-// Listen
-db.sequelize.sync(SYNC_OPTIONS).then(() => {
-  app.listen(PORT, () => {
-    console.log(`App listening on port: ${PORT}`);
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
-});
+}
+
+// Dynamically force schema refresh only for 'test'
+const FORCE_SCHEMA = process.env.NODE_ENV === "test";
+
+db.sequelize
+  .authenticate()
+  .then(() => {
+    db.sequelize.sync({ force: FORCE_SCHEMA }).then(() => {
+      app.listen(PORT, (err) => {
+        if (err) throw err;
+        console.log(
+          `🌎 Server is Ready and Listening on http://localhost:${PORT}`
+        ); // eslint-disable-line no-console
+      });
+    });
+  })
+  .catch(console.error); // eslint-disable-line no-console
+
+module.exports = app;
