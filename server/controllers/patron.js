0e09fb92345033aacc5853490f00c0b7eb1be8cd
Greg Stead <gregstead@gmail.com>
2021-06-06 21:10:27 -0500

    adds patron controller and patron db seed

---
+++ server/controllers/patron.js
@@ -0,0 +1,42 @@
+const db = require("../models");
+
+const patron = {
+  findAllPatrons: function(_req, res) {
+    db.Patron.findAll()
+      .then((result) => res.json(result))
+      .catch((err) => res.status(500).json(err));
+  },
+  findRoleById: function(req, res) {
+    db.Patron.findById(req.params.id)
+      .then((result) => res.json(result))
+      .catch((err) => res.status(500).json(err));
+  },
+  createNewPatron: function(req, res) {
+    db.Patron.create(req.body)
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+  updatePatron: async function(req, res) {
+    db.Patron.update(req.body, {
+      where: {
+        id: req.body.id,
+      },
+    })
+      .then((result) => res.json(result))
+      .catch((err) => res.status(500).json(err));
+  },
+  deletePatron: async function(req, res) {
+    const { id } = req.body;
+    db.Patron.destroy({
+      where: {
+        id: id,
+      },
+    })
+      .then((result) => res.json(result))
+      .catch((err) => res.status(500).json(err));
+  },
+};
+
+module.exports = patron;
---
+++ server/database/seeds/patron.js
@@ -0,0 +1,23 @@
+const db = require("../../modelss");
+
+const patrons = [
+  {
+    title: "mr",
+    first_name: "Gregory",
+    middle_name: "Trevor",
+    last_name: "Stead",
+    phone: "555-555-5555",
+    phone2: "555-555-5555",
+    phone3: "555-555-5555",
+    email: "gregstead@gmail.com",
+    website: "gregstead.github.io",
+    linkedIn: "linkedin.com/in/gregstead",
+    twitter: "twitter.com/soylentgreg",
+    facebook: "none",
+    birthday: "1989-02-22",
+  },
+];
+
+module.exports = function() {
+  patrons.forEach((patron) => {});
+};
