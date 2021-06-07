0a06f3b9a46fdde2f498fcb03414939a3c10b16c
Greg Stead <gregstead@gmail.com>
2021-06-07 11:25:44 -0500

    removes commented out code

--- server/seeders/20210607133903-patron.js
+++ server/seeders/20210607133903-patron.js
@@ -2,26 +2,6 @@
 
 const { query } = require("express");
 
-// const patrons = require("./randomPeople.json").results.map((patron) => {
-//   return {
-//     title: patron.name.title,
-//     first_name: patron.name.first,
-//     middle_name: null,
-//     last_name: patron.name.last,
-//     phone: patron.phone,
-//     phone2: patron.cell,
-//     phone3: null,
-//     email: patron.email,
-//     website: "website.com",
-//     linkedIn: "linkedin.com",
-//     twitter: "twitter.com",
-//     facebook: "facebook.com",
-//     birthday: patron.birthday,
-//     createdAt: "2020-12-01",
-//     updatedAt: "2021-06-07",
-//   };
-// });
-
 const addresses = require("./randomPeople.json").results.map((location) => {
   return {
     line1:
