7a3151a15e18a8adda4a75c26c60aafe7370245b
Greg Stead <gregstead@gmail.com>
2021-06-07 11:43:13 -0500

    removes comments and unreachable code

--- server/migrations/20210607133857-patron.js
+++ server/migrations/20210607133857-patron.js
@@ -1,13 +1,66 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable("addresses", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: DataTypes.STRING,
+      },
+      first_name: {
+        type: DataTypes.STRING,
+      },
+      middle_name: {
+        type: DataTypes.STRING,
+      },
+      last_name: {
+        type: DataTypes.STRING,
+      },
+      phone: {
+        type: DataTypes.STRING,
+      },
+      phone2: {
+        type: DataTypes.STRING,
+      },
+      phone3: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      website: {
+        type: DataTypes.STRING,
+      },
+      linkedIn: {
+        type: DataTypes.STRING,
+      },
+      twitter: {
+        type: DataTypes.STRING,
+      },
+      facebook: {
+        type: DataTypes.STRING,
+      },
+      birthday: {
+        type: DataTypes.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -17,5 +70,5 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
-  }
+  },
 };
