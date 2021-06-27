df332df2f1374d0ebed5d81ae2a5cf4470fc9731
gregstead <gregstead@gmail.com>
2021-06-27 02:00:09 -0500

    basic user reg page

--- client/src/pages/AddNewUser/index.jsx
+++ client/src/pages/AddNewUser/index.jsx
@@ -1,10 +1,71 @@
-// import React from "react"
-// import NewUserForm from "../../components/Forms/SignUpForm"
+import React, { useState } from "react"
+import API from "../../utils/API"
 
-// function NewUserPage() {
- 
+function NewUserPage() {
+    const [userState, setUserState] = useState({
+        email: "",
+        password: "",
+        first_name: "",
+        last_name: "",
+        
+    })
 
-//   return (<NewUserForm />)
-//    }
+    function handleChange(event) {
+        const {name, value } = event.target;
+        setUserState({...userState, [name]: value})
+    }
 
-// export default NewUserPage;
+    function handleSubmit(event) {
+        event.preventDefault();
+        API.createNewUser(userState);
+    }
+
+    return (<form onSubmit={handleSubmit}>
+        <label>
+        First name:
+        <input
+          type="text"
+          name="firstName"
+          value={userState.firstName}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Last name:
+        <input
+          type="text"
+          name="lastName"
+          value={userState.lastName}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="text"
+          name="email"
+          value={userState.email}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          name="password"
+          value={userState.password}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Confirm Password:
+        <input
+          type="password"
+          name="password"
+          // onChange=check password match
+        />
+      </label>
+    </form>)
+}
+
+export default NewUserPage;
