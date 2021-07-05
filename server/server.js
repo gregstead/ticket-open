17e8c05a89f6e30886299916e1edc00f181a7049
gregstead <gregstead@gmail.com>
2021-07-05 18:05:16 -0500

    changes for heroku deployment

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const session = require("express-session");
 
 const db = require("./models");
 const routes = require("./routes");
-const passport = require("./config/passport");
+// const passport = require("./config/passport");
 // const corsOptions = require("./config/cors.js");
 
 const PORT = process.env.PORT || 3001;
@@ -17,8 +17,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(helmet());
 app.use(session({ secret: "TBD", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.initialize());
+//app.use(passport.session());
 // app.use(cors(corsOptions));
 
 // Serve up static assets
@@ -34,7 +34,7 @@ app.use(routes);
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index/html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
